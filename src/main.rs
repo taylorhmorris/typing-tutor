9e2ee896c1d6a8f133a311674c04861fc355f703
tmorris <tmorris@student.42.fr>
2022-07-24 00:30:27 +0200

    feat: update program to run README file
    - Loads README as typing phrases instead of a single hardcoded phrase

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::io::{self, Write};
-use console::{Term, style};
+use console::{style, Term};
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+use std::path::Path;
 
-struct Phrase<'a> {
-	content: &'a str,
+struct Phrase {
+	content: String,
 	index: usize,
 }
 
-impl Phrase<'_> {
+impl Phrase {
 	fn handle_input(&mut self, character: char) -> bool {
 		if self.index >= self.content.len() {
 			return true;
@@ -15,15 +17,14 @@ impl Phrase<'_> {
 			print!("{}", style(character).green());
 			io::stdout().flush().unwrap();
 			self.index += 1;
-		}
-		else {
+		} else {
 			print!("{}", style(character).red());
 			io::stdout().flush().unwrap();
 			let stdout = Term::stdout();
 			stdout.move_cursor_left(1).unwrap();
 		}
 		if self.index >= self.content.len() {
-			return true;	
+			return true;
 		}
 		return false;
 	}
@@ -31,20 +32,45 @@ impl Phrase<'_> {
 
 fn main() {
 	let stdout = Term::buffered_stdout();
-	let mut phrase = Phrase { content: "Hello, World!", index: 0 };
+	let mut phrase = Phrase {
+		content: "".to_string(),
+		index: 0,
+	};
 
-	println!("{}", phrase.content);
-	'game_loop: loop {
-		if let Ok(character) = stdout.read_char() {
-			match character {
-				'`' => break 'game_loop,
-				_ => {
-						if phrase.handle_input(character) {
-							println!("You won!");
-							break 'game_loop;
+	if let Ok(lines) = read_lines("./README.md") {
+		for line in lines {
+			phrase.index = 0;
+			if let Ok(line_str) = line {
+				phrase.content = line_str.clone();
+			} else {
+				println!("{}", style("Invalid Line Detected! Skipping").yellow());
+				continue;
+			}
+			println!("{}", phrase.content);
+			'game_loop: loop {
+				if let Ok(character) = stdout.read_char() {
+					match character {
+						'`' => break 'game_loop,
+						_ => {
+							if phrase.handle_input(character) {
+								println!("");
+								break 'game_loop;
+							}
 						}
-					},
+					}
+				}
 			}
 		}
+		println!("\n{}", style("COMPLETED!").green());
+	} else {
+		println!("Invalid File");
 	}
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+	P: AsRef<Path>,
+{
+	let file = File::open(filename)?;
+	Ok(io::BufReader::new(file).lines())
 }
